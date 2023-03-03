a2403b4bc6024ecfd5bc85054ce543d2f6ac2be7
JairoARH <jairo.riano@uptc.edu.co>
2023-03-03 18:21:22 -0500

    Cambios marzo 2023

--- controllers/controll-bills.js
+++ controllers/controll-bills.js
@@ -21,19 +21,26 @@ module.exports = {
 
                 bill.customer = customer
 
+                console.log(req.body)
+
                 const result = await bill.save()
+
+                console.log('Hechooo')
 
                 customer.bills.push(bill)
 
                 await customer.save()
+
                 console.log("Done")
 
                 return res.status(200).json({"result":true,"data":result})
 
             }else{
-                return res.status(200).json({"result":false,"error":"No existe el Cliente"})  
+                console.log('Dizque no existe')
+                return res.status(500).json({"result":false,"error":"No existe el Cliente"})  
             }
         } catch (error) {
+            console.log('Por el lado del error')
             return res.status(500).json({"result":false,"error":error}) 
         }
 
--- drivers/connect-db.js
+++ drivers/connect-db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb://localhost:27017/sales'
+const URI = 'mongodb://127.0.0.1:27017/sales'
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(URI)
+const options = {
+    useNewUrlParser:true, useUnifiedTopology:true
+}
+
+mongoose.connect(URI,options)
     .then(()=>console.log('Connect DB Success'))
     .catch( e => console.log(e))
 
